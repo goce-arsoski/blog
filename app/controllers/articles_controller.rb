e90e9e67a1ba22c325925809bc04416357183882
goce-arsoski <goce_ars@yahoo.com>
2020-05-09 20:10:36 +0200

    Add edit method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,13 +12,26 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    binding.pry
     @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
   end
 
