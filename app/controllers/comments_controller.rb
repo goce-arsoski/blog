297bd24952f5b9e3e92360173f6bc721121f4c92
goce-arsoski <goce_ars@yahoo.com>
2020-06-06 15:21:14 +0200

    Use Fontawesome from a gem && delete an article comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,34 @@ class CommentsController < ApplicationController
 
     comment = article.comments.build(comment_params)
 
-    comment.save
-    redirect_to article
+    if comment.save
+      redirect_to article
+    else
+      render 'article/show'
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+
+    redirect_to comment.article
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+
+    @article = @comment.article
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+
+    if comment.update(comment_params)
+      redirect_to comment.article
+    else
+      render :edit
+    end
   end
 
   private
