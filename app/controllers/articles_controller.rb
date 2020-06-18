a381a684291133699fb4242d5627719a4261b650
goce-arsoski <goce_ars@yahoo.com>
2020-06-18 18:19:52 +0200

    New Users Controller with :new, :create and :show actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,6 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    @comments = @article.comments.order(id: :desc)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
+
   resources :articles do
     resources :comments
   end
 
-  root to: 'articles#index'
+  get 'users/signup', to: 'users#new'
+  resources :users, except: :new
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
