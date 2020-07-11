68779f9c1ddfbcab171679a64e70ca0a8a28daef
goce-arsoski <goce_ars@yahoo.com>
2020-07-11 19:06:51 +0200

    New system specs for Article interaction

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'support/factory_bot'
-require 'support/shoulda_matchers.rb'
+require 'support/shoulda_matchers'
+require 'helpers/user_helper'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -62,4 +64,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  #Include custom modules
+  config.include UserHelper
 end
